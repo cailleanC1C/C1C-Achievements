013dcfaa7bcffa2c9d0bb5e4c4d35aa9ccf71159
Caillean <caillean.games@gmail.com>
2025-10-03 01:07:47 +0200

    Update __init__.py

--- cogs/shards/__init__.py
+++ cogs/shards/__init__.py
@@ -1,7 +1,5 @@
-*** /dev/null
---- a/cogs/shards/__init__.py
-@@
-+from .cog import ShardsCog
-+
-+async def setup(bot):
-+    await bot.add_cog(ShardsCog(bot))
+from .cog import ShardsCog
+
+async def setup(bot):
+    await bot.add_cog(ShardsCog(bot))
+
